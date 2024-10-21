149aae425ed33879a1be0c578be8143b0fe9d03e
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-10-21 10:40:55 +0100

    Update copyright year in footer from 2023 to 2024

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -352,7 +352,7 @@ export function PortfolioComponent() {
 
       <footer className="bg-gray-800 text-white py-8">
         <div className="container mx-auto px-4 text-center">
-          <p>&copy; 2023 Maxamed Maxamed. All rights reserved.</p>
+          <p>&copy; 2024 Maxamed Maxamed. All rights reserved.</p>
         </div>
       </footer>
     </div>
