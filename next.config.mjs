3d54be8f2ada11c3ed3e8e50a4df5e218deab0d1
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-10-22 19:58:40 +0100

    Update next.config.mjs and package.json files

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
     output: 'export',
     basePath: '/myportfolio',
+    assetPrefix: '/myportfolio/',
+    trailingSlash: true,
     images: {
       unoptimized: true,
       loader: 'custom',
       loaderFile: './imageLoader.js',
     },
   };
-  
   
   export default nextConfig;  
